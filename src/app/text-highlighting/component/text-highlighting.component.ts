ff0dc43d05f1d14311f149b416db3e9d82799f3c
AElMehdi <assirelmehdi@gmail.com>
2019-11-22 23:02:22 +0100

    Toggle the side-nav on text selection

--- src/app/text-highlighting/component/text-highlighting.component.ts
+++ src/app/text-highlighting/component/text-highlighting.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-text-highlighting',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextHighlightingComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('sideNav') sideNav;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
@@ -15,6 +18,7 @@ export class TextHighlightingComponent implements OnInit {
   handleMouseUp() {
     if (window.getSelection().toString()) {
       console.log('Show a popup');
+      this.sideNav.toggle();
     }
   }
 }
