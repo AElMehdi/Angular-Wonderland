714cebcfc690214cc9e603857943590bd7869f18
AElMehdi <assirelmehdi@gmail.com>
2020-07-07 23:39:11 +0200

    Basic chord chart

--- src/app/scored-against-stat/scored-against-stat.component.ts
+++ src/app/scored-against-stat/scored-against-stat.component.ts
@@ -1,9 +1,5 @@
-import { AfterContentInit, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
-import * as d3Scale from 'd3-scale';
-import * as d3Shape from 'd3-shape';
-import * as d3Array from 'd3-array';
-import * as d3Axis from 'd3-axis';
 
 @Component({
   selector: 'app-scored-against-stat',
@@ -12,68 +8,73 @@ import * as d3Axis from 'd3-axis';
   encapsulation: ViewEncapsulation.None
 })
 export class ScoredAgainstStatComponent implements OnInit {
-  title = 'Line Chart';
-  data: any[] = [
-    {date: new Date('2010-01-01'), value: 80},
-    {date: new Date('2010-01-04'), value: 90},
-    {date: new Date('2010-01-05'), value: 95},
-    {date: new Date('2010-01-06'), value: 100},
-    {date: new Date('2010-01-07'), value: 110},
-    {date: new Date('2010-01-08'), value: 120},
-    {date: new Date('2010-01-09'), value: 130},
-    {date: new Date('2010-01-10'), value: 140},
-    {date: new Date('2010-01-11'), value: 150},
-  ];
-
-  private margin = {top: 20, right: 20, bottom: 30, left: 50};
-  private width: number;
-  private height: number;
-  private x: any;
-  private y: any;
   private svg: any;
-  private line: d3Shape.Line<[number, number]>; // this is line defination
 
   constructor() {
-    // configure margins and width/height of the graph
-
-    this.width = 960 - this.margin.left - this.margin.right;
-    this.height = 500 - this.margin.top - this.margin.bottom;}
+  }
 
   ngOnInit() {
-    this.buildSvg();
-    this.addXandYAxis();
-    this.drawLineAndPath();
-  }
-  private buildSvg() {
+    // create the svg area
     this.svg = d3.select('svg')
+      .append('svg')
+      .attr('width', 440)
+      .attr('height', 440)
       .append('g')
-      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
-  }
-  private addXandYAxis() {
-    // range of data configuring
-    this.x = d3Scale.scaleTime().range([0, this.width]);
-    this.y = d3Scale.scaleLinear().range([this.height, 0]);
-    this.x.domain(d3Array.extent(this.data, (d) => d.date ));
-    this.y.domain(d3Array.extent(this.data, (d) => d.value ));
+      .attr('transform', 'translate(220,220)');
 
-    // Configure the Y Axis
-    this.svg.append('g')
-      .attr('transform', 'translate(0,' + this.height + ')')
-      .call(d3Axis.axisBottom(this.x));
-    // Configure the Y Axis
-    this.svg.append('g')
-      .attr('class', 'axis axis--y')
-      .call(d3Axis.axisLeft(this.y));
-  }
+// create a matrix
+    const matrix = [
+      [0, 5871, 8916, 2868],
+      [1951, 0, 2060, 6171],
+      [8010, 16145, 0, 8045],
+      [1013, 990, 940, 0]
+    ];
 
-  private drawLineAndPath() {
-    this.line = d3Shape.line()
-      .x( (d: any) => this.x(d.date) )
-      .y( (d: any) => this.y(d.value) );
-    // Configuring line path
-    this.svg.append('path')
-      .datum(this.data)
-      .attr('class', 'line')
-      .attr('d', this.line);
+// 4 groups, so create a vector of 4 colors
+    const colors = ['#440154ff', '#31668dff', '#37b578ff', '#fde725ff'];
+
+// give this matrix to d3.chord(): it will calculates all the info we need to draw arc and ribbon
+    const res = d3.chord()
+      .padAngle(0.05)
+      .sortSubgroups(d3.descending)
+      (matrix);
+
+// add the groups on the outer part of the circle
+    this.svg
+      .datum(res)
+      .append('g')
+      .selectAll('g')
+      .data(function (d) {
+        return d.groups;
+      })
+      .enter()
+      .append('g')
+      .append('path')
+      .style('fill', function (d, i) {
+        return colors[i];
+      })
+      .style('stroke', 'black')
+      .attr('d', d3.arc()
+        .innerRadius(200)
+        .outerRadius(210)
+      );
+
+// Add the links between groups
+    this.svg
+      .datum(res)
+      .append('g')
+      .selectAll('path')
+      .data(function (d) {
+        return d;
+      })
+      .enter()
+      .append('path')
+      .attr('d', d3.ribbon()
+        .radius(200)
+      )
+      .style('fill', function (d) {
+        return (colors[d.source.index]);
+      }) // colors depend on the source group. Change to target otherwise.
+      .style('stroke', 'black');
   }
 }
