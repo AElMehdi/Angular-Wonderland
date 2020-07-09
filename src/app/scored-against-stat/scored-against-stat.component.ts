c69fded4d5714c47924e187bfb84df0dd18a3b2f
AElMehdi <assirelmehdi@gmail.com>
2020-07-09 09:54:51 +0200

    Add labels to D3 Chords

--- src/app/scored-against-stat/scored-against-stat.component.ts
+++ src/app/scored-against-stat/scored-against-stat.component.ts
@@ -17,10 +17,10 @@ export class ScoredAgainstStatComponent implements OnInit {
     // create the svg area
     this.svg = d3.select('svg')
       .append('svg')
-      .attr('width', 440)
-      .attr('height', 440)
+      .attr('width', 600)
+      .attr('height', 600)
       .append('g')
-      .attr('transform', 'translate(220,220)');
+      .attr('transform', 'translate(300,300)');
 
 // create a matrix
     const matrix = [
@@ -32,6 +32,7 @@ export class ScoredAgainstStatComponent implements OnInit {
 
 // 4 groups, so create a vector of 4 colors
     const colors = ['#440154ff', '#31668dff', '#37b578ff', '#fde725ff'];
+    const names = ['#440154ff', '#31668dff', '#37b578ff', '#fde725ff'];
 
 // give this matrix to d3.chord(): it will calculates all the info we need to draw arc and ribbon
     const res = d3.chord()
@@ -41,10 +42,70 @@ export class ScoredAgainstStatComponent implements OnInit {
 
 
 // add the groups on the outer part of the circle
-    this.svg.selectAll('g.group')
-      .data(res)
-      .enter().append('svg:g')
-      .attr('class', d => 'group ' + names[d.index]);
+//     this.svg.selectAll('g.group')
+//       .data(res)
+//       .enter().append('svg:g')
+//       .attr('class', d => 'group ' + names[d.index]);//Append the label names on the outside
+
+    // outerArcs.append("text")
+    //   .attr("class", "titles")
+    //   .attr("dy", function(d,i) { return (d.endAngle > 90*Math.PI/180 & d.startAngle < 270*Math.PI/180 ? 25 : -16); })
+    //   .append("textPath")
+    //   .attr("startOffset","50%")
+    //   .style("text-anchor","middle")
+    //   .attr("xlink:href",function(d,i){return "#arc"+i;})
+    //   .text(function(d,i){ return Names[i]; });
+
+    const textLabel = d3.scaleOrdinal().range(['Black', 'Blonde', 'Brown', 'Red']);
+    //Create an SVG path (based on bl.ocks.org/mbostock/2565344)
+    this.svg.append("path")
+      .attr("id", "wavy") //Unique id of the path
+      .attr("d", "M 10,90 Q 100,15 200,70 Q 340,140 400,30") //SVG path
+      .style("fill", "none")
+      .style("stroke", "#AAAAAA");
+
+  //Create an SVG text element and append a textPath element
+    this.svg.append("text")
+      .append("textPath") //append a textPath to the text element
+      .attr("xlink:href", "#wavy") //place the ID of the path here
+      .style("text-anchor","middle") //place the text halfway on the arc
+      .attr("startOffset", "50%")
+      .text("Yay, my text is on a wavy path");
+
+    this.svg.append('g')
+      .selectAll('path')
+      .data(res.groups)
+      .enter()
+      .append('path')
+      .attr('id', (d, i) => 'group-' + i)
+      .style('fill', 'white')
+      .style('stroke', 'white')
+      .attr('d', d3.arc().innerRadius(220).outerRadius(230));
+
+    this.svg.append('g')
+      .selectAll('text')
+      .data(res.groups)
+      .enter()
+      .append('text')
+      .attr('x', 6)
+      .attr('dy', 15)
+      .append('textPath')
+      .style('fill', (d, i) => colors[i])
+      .style('stroke', 'black')
+      .attr('xlink:href', (d, i) => '#group-' + i)
+      .style('text-anchor', 'middle') //place the text halfway on the arc
+      .attr('startOffset', '80%')
+      .text((d, i) => textLabel(i));
+
+    // this.svg
+    //   .append('text')
+    //   .attr('x', 6)
+    //   .attr('dy', 15)
+    //   // .filter(d => d.value > 110)
+    //   .append('textPath')
+    //   .attr('xlink:href', (d, i) => '#group' + i)
+    //   .text((d, i) => textLabel[i]);
+
 
     this.svg
       .datum(res)
